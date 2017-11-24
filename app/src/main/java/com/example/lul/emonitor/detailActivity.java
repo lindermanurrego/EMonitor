c3332209072b88f5b61701659a7eef50b450b908
lindermanurrego <lindermanu@gmail.com>
2017-11-24 07:41:37 -0500

    Commit

--- app/src/main/java/com/example/lul/emonitor/detailActivity.java
+++ app/src/main/java/com/example/lul/emonitor/detailActivity.java
@@ -3,6 +3,7 @@ package com.example.lul.emonitor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class detailActivity extends AppCompatActivity {
 
     @Override
