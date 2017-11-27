4c3dc70d3c9e9e54bb9ede3dfa59300c365f750d
lindermanurrego <lindermanu@gmail.com>
2017-11-27 11:14:10 -0500

    Commit

--- app/src/main/java/EqDbHelper.java
+++ app/src/main/java/EqDbHelper.java
@@ -18,7 +18,7 @@ public class EqDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-             String EARTHQUAKES_DATABASE = "CREATE TABLE " + DATABASE_NAME + "("+
+             String EARTHQUAKES_DATABASE = "CREATE TABLE " + EqContract.EqColumns.TABLE_NAME + "("+
                      EqContract.EqColumns._ID + " INTEGER PRIMARI KEY AUTOINCREMENT, " +
                      EqContract.EqColumns.MAGNITUDE + " REAL NOT NULL, " +
                      EqContract.EqColumns.PLACE + " TEXT NOT NULL, " +
