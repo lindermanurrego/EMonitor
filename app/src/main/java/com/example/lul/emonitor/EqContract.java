a3c54c80fe51904e1772ef14b826bd627543f8e6
lindermanurrego <lindermanu@gmail.com>
2017-12-03 21:28:15 -0500

    Commit

--- app/src/main/java/com/example/lul/emonitor/EqContract.java
+++ app/src/main/java/com/example/lul/emonitor/EqContract.java
@@ -15,5 +15,11 @@ public class EqContract {
         public static final String LONGITUDE = "longitude";
         public static final String LATITUDE = "latitude";
         public static final String TIME = "time";
+
+        public static final int MAGNITUDE_INDEX = 1;
+        public static final int PLACE_INDEX = 2;
+        public static final int LONGITUDE_INDEX = 3;
+        public static final int LATITUDE_INDEX = 4;
+        public static final int TIME_INDEX = 5;
     }
 }
--- app/src/main/java/com/example/lul/emonitor/MainActivity.java
+++ app/src/main/java/com/example/lul/emonitor/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.lul.emonitor;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -37,22 +39,50 @@ public class MainActivity extends AppCompatActivity implements DownloadEqAsyncTa
        earthquakeListView = findViewById(R.id.earthquake_list_view);
 
        if(Utils.isNetworwAvailable(this)) {
-           DownloadEqAsyncTask downloadEqAsyncTask = new DownloadEqAsyncTask(this);
-           downloadEqAsyncTask.delegate = this;
-           try {
-               downloadEqAsyncTask.execute(new URL("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"));
-           } catch (MalformedURLException e) {
-               e.printStackTrace();
-           }
+         downloadEarthQuakes();
        }else{
            //Leer la base de datos
-
+          getEarthQuakesFromDb();
        }
 
     }
+
+    private void getEarthQuakesFromDb(){
+        EqDbHelper eqDbHelper = new EqDbHelper(this);
+        SQLiteDatabase database  = eqDbHelper.getReadableDatabase();
+
+        Cursor cursor = database.query(EqContract.EqColumns.TABLE_NAME,null,null,null,null,null,null);
+        ArrayList<Earthquake> eqList = new ArrayList<>();
+
+
+        while (cursor.moveToNext()){
+            Double magnitude = cursor.getDouble(EqContract.EqColumns.MAGNITUDE_INDEX);
+            String place = cursor.getString(EqContract.EqColumns.PLACE_INDEX) ;
+            Long time = cursor.getLong(EqContract.EqColumns.TIME_INDEX);
+            String longitude = cursor.getString(EqContract.EqColumns.LONGITUDE_INDEX);
+            String latitude = cursor.getString(EqContract.EqColumns.LATITUDE_INDEX);
+
+            eqList.add(new Earthquake(magnitude,place,time,longitude,latitude));
+
+        }
+        cursor.close();
+        fillEqList(eqList);
+    }
+    private void downloadEarthQuakes(){
+        DownloadEqAsyncTask downloadEqAsyncTask = new DownloadEqAsyncTask(this);
+        downloadEqAsyncTask.delegate = this;
+        try {
+            downloadEqAsyncTask.execute(new URL("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public void onEqsDownloaded(ArrayList<Earthquake> eqList) {
+         fillEqList(eqList);
+    }
 
+    private void fillEqList(ArrayList<Earthquake> eqList){
         final EqAdapter eqAdapter = new EqAdapter(this,R.layout.eq_list_item,eqList);
         earthquakeListView.setAdapter(eqAdapter);
 
